7e958c08b107c0ae6df2fe062368b9ba238de7cf
Ovidijus Stankaitis <ovidijus.stankaitis@gmail.com>
2022-11-02 02:20:46 +0200

    Started 7.6

---
+++ Paskaitoms/7.6/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int showMenu(char *menuTitle, char *menuOptions[], int menuSize, char *inputMsg);
+
+int main()
+{
+
+    return 0;
+}
+
+int showMenu(char *menuTitle, char *menuOptions[], int menuSize, char *inputMsg)
+{
+
+}
