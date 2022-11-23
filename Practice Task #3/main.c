a0c34538e144ede52eb355d633f720354bb8a3ba
Ovidijus Stankaitis <ovidijus.stankaitis@gmail.com>
2022-11-23 22:53:49 +0200

    Added input read function from user provided file

--- Practice Task #3/main.c
+++ Practice Task #3/main.c
@@ -1,16 +1,55 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define fileLength 256
+#define fileNoExtLength 252
+#define textLength 256
 
-FILE* fileValidation(FILE* file, char fileName[])
+FILE* fileValidation(FILE* file);
+void inputRead(char*** text, FILE* input, int* size);
+
+int main()
 {
-    scanf("%s", fileName);
+    FILE* input = NULL;
+
+    input = fileValidation(input);
+
+    char** text;
+
+    int size;
+
+    inputRead(&text, input, &size);
+
+    for(int i = 0; i < size; i++)
+    {
+        printf("%d\n", strlen(text[i]));
+    }
+
+    free(text);
+    return 0;
+}
+
+FILE* fileValidation(FILE* file)
+{    
+    char fileName[fileLength];
+    char fileNoExtName[fileNoExtLength];
+
+    printf("Please enter an input file name without extension:\n");
+    printf("The program will search for a .txt file with the name you provided\n");
+    scanf("%s", fileNoExtName);
+
+    snprintf(fileName, sizeof(fileName), "%s.txt", fileNoExtName);
+
     file = fopen(fileName, "r");
 
     while(file == NULL)
     {
-        printf("File not found. Please enter an input file name with extension:\n");
-        scanf("%s", fileName);
+        printf("File not found. Please enter an input file name without extension:\n");
+        printf("The program will search for a .txt file with the name you provided\n");
+        scanf("%s", fileNoExtName);
+    
+        snprintf(fileName, sizeof(fileName), "%s.txt", fileNoExtName);
 
         file = fopen(fileName, "r");
     }
@@ -18,17 +57,38 @@ FILE* fileValidation(FILE* file, char fileName[])
     return file;
 }
 
-int main()
+void inputRead(char*** text, FILE* input, int* size)
 {
-    FILE* input = NULL;
-    FILE* output = NULL;
+    *size = 1;
 
-    char fileName[fileLength];
-    char random[10];
-    
-    printf("Please enter an input file name with extension:\n");
+    (*text) = malloc(sizeof(char*) * (*size));
 
-    input = fileValidation(input, fileName);
+    int i = 0;
 
-    return 0;
+
+    while(!feof(input))
+    {
+        (*text)[i] = malloc(sizeof(char) * textLength);
+
+        fgets((*text)[i], textLength, input);
+
+        if(strlen((*text)[i]) == (textLength - 1) && fgetc(input) != '\n')
+        {
+            while(fgetc(input) != '\n' && !feof(input))
+            {
+                ;
+            }
+        }
+
+        if(!feof(input))
+        {
+            i++;
+
+            if(i >= (*size))
+            {
+                (*size)++;
+                (*text) = realloc((*text), sizeof(char*) * (*size));
+            }
+        }
+    }
 }
