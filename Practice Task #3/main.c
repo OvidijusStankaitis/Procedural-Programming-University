47fd3d7e62d090f325d1eb4006c5abe7af3d0a72
Ovidijus Stankaitis <ovidijus.stankaitis@gmail.com>
2022-11-24 03:00:03 +0200

    Starting to fix string parsing

--- Practice Task #3/main.c
+++ Practice Task #3/main.c
@@ -7,26 +7,36 @@
 #define textLength 256
 
 FILE* fileValidation(FILE* file);
+
 void inputRead(char*** text, FILE* input, int* size);
+
+void parsingInput(char*** out, char** text, int size);
 
 int main()
 {
     FILE* input = NULL;
 
-    input = fileValidation(input);
-
     char** text;
+    char** out;
 
-    int size;
+    int size = 0;
+
+    input = fileValidation(input);
 
     inputRead(&text, input, &size);
 
-    for(int i = 0; i < size; i++)
-    {
-        printf("%d\n", strlen(text[i]));
-    }
+    parsingInput(&out, text, size);
 
+    // for(int i = 0; i < size; i++)
+    // {
+    //     printf("%s\n", out[i]);
+    // }
+
+    fclose(input);
+
+    free(out);
     free(text);
+
     return 0;
 }
 
@@ -63,14 +73,25 @@ void inputRead(char*** text, FILE* input, int* size)
 
     (*text) = malloc(sizeof(char*) * (*size));
 
-    int i = 0;
-
+    int i = 0, n = 0;
 
     while(!feof(input))
     {
         (*text)[i] = malloc(sizeof(char) * textLength);
 
         fgets((*text)[i], textLength, input);
+
+        n = strlen((*text)[i]);
+
+        if(n < textLength)
+        {
+            (*text)[i] = realloc((*text)[i], sizeof(char) * (n + 1));
+        }
+
+        if((*text)[i][n - 1] == '\n')
+        {
+            (*text)[i][n - 1] = '\0';
+        }
 
         if(strlen((*text)[i]) == (textLength - 1) && fgetc(input) != '\n')
         {
@@ -91,4 +112,64 @@ void inputRead(char*** text, FILE* input, int* size)
             }
         }
     }
+}
+
+void parsingInput(char*** out, char** text, int size)
+{
+    int n = 0, i = 0;
+
+    (*out) = malloc(sizeof(char*) * size);
+
+    while(i < size)
+    {
+        int index = 0;
+
+        char ARR[256];
+
+        char* token = strtok(text[i], " ");
+
+        while(token != NULL)
+        {
+            n = strlen(token);
+
+            if(token[1] == token[n - 3])
+            {
+                for(int u = 0; u < n; u++)
+                {
+                    ARR[index + u] = token[u];
+                }
+
+                index += n;
+
+                ARR[index++] = ' ';                           
+            }
+
+            token = strtok(NULL, " ");
+        }
+
+        (*out)[i] = malloc(sizeof(char) * strlen(ARR));
+
+        if(strlen(ARR) == 0)
+        {
+            (*out)[i] = "-";
+        }
+
+        else 
+        {
+            (*out)[i] = ARR;
+        }
+
+        printf("%s\n", ARR);
+
+        memset(ARR, 0, sizeof(ARR));
+
+        // printf("%s\n", ARR);
+
+        i++;
+    }   
+
+    // for(int i = 0; i < size; i++)
+    // {
+    //     printf("%s\n", (*out)[i]);
+    // }
 }
