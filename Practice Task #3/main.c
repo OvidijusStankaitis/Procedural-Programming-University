50a3355f6c789f5621a7507e2293443aafb2c44b
Ovidijus Stankaitis <ovidijus.stankaitis@gmail.com>
2022-11-27 23:03:11 +0200

    Finished practice task 3

--- Practice Task #3/main.c
+++ Practice Task #3/main.c
@@ -1,12 +1,19 @@
+// LSP numeris 2213794
+// ovidijus.stankaitis@mif.stud.vu.lt
+// Užduoties nr. - 3
+// Variantas nr. - 5
+
+// Including libraries
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
 
+// Defining constants
 #define fileLength 256
 #define fileNoExtLength 252
 #define textLength 256
 
+// Function/Procedure declarations
 FILE* fileValidation(FILE* file, char* mode);
 
 void inputRead(char*** text, FILE* input, int* size);
@@ -25,9 +32,11 @@ int main()
 
     int size = 0;
 
+    // Prompts user to enter input file name and checks if it exits or not adn opens it
     printf("Please enter an input file name without extension:\n");
     input = fileValidation(input, "r");
 
+    // Checks if file is empty, if it is - program is aborted
     fseek(input, 0L, SEEK_END);
     if(ftell(input) == 0)
     {
@@ -36,6 +45,7 @@ int main()
     }
     fseek(input, 0L, SEEK_SET);
 
+    // Prompts user to enter output file name, if it exits - opens file if not - creates a file
     printf("Please enter an output file name without extension:\n");
     output = fileValidation(output, "w");
 
@@ -62,10 +72,12 @@ FILE* fileValidation(FILE* file, char* mode)
     printf("The program will search for a .txt file with the name you provided\n");
     scanf("%s", fileNoExtName);
 
+    // Joins entered file name with no extension wit ".txt"
     snprintf(fileName, sizeof(fileName), "%s.txt", fileNoExtName);
 
     file = fopen(fileName, mode);
 
+    // Checks if file was opened
     while(file == NULL)
     {
         printf("File not found. Please enter a file name without extension:\n");
@@ -77,6 +89,7 @@ FILE* fileValidation(FILE* file, char* mode)
         file = fopen(fileName, mode);
     }
 
+    // Returns the file
     return file;
 }
 
@@ -84,28 +97,34 @@ void inputRead(char*** text, FILE* input, int* size)
 {
     *size = 1;
 
+    // Allocates 1 line to the buffer
     (*text) = malloc(sizeof(char*) * (*size));
 
     int i = 0, n = 0;
 
+    // While not EOF
     while(!feof(input))
     {
+        // Allocates 256 chars to a line
         (*text)[i] = malloc(sizeof(char) * textLength);
 
         fgets((*text)[i], textLength, input);
 
         n = strlen((*text)[i]);
 
-        if(n < textLength)
+        // If text length is less than 255, buffer is reduced to n chars
+        if(n < (textLength - 1))
         {
             (*text)[i] = realloc((*text)[i], sizeof(char) * (n + 1));
         }
 
+        // Endline is replaced with a string end symbol
         if((*text)[i][n - 1] == '\n')
         {
             (*text)[i][n - 1] = '\0';
         }
 
+        // If line has more than 255 symbols it ignores it
         if(strlen((*text)[i]) == (textLength - 1) && fgetc(input) != '\n')
         {
             while(fgetc(input) != '\n' && !feof(input))
@@ -114,6 +133,7 @@ void inputRead(char*** text, FILE* input, int* size)
             }
         }
 
+        // Allocates +1 line to the dynamic buffer if not EOF
         if(!feof(input))
         {
             i++;
@@ -131,6 +151,7 @@ void parsingInput(char*** out, char** text, int size)
 {
     int n = 0, i = 0;
 
+    // Allocating the number of lines to the dynamic buffer
     (*out) = malloc(sizeof(char*) * size);
 
     while(i < size)
@@ -140,17 +161,18 @@ void parsingInput(char*** out, char** text, int size)
         char ARR[256];
         memset(ARR, 0, sizeof(ARR));
 
+        // Separating string into tokens of a single word
         char* token = strtok(text[i], " ");
-
-        bool temp = false;
 
         while(token != NULL)
         {
             n = strlen(token);
 
+            // Checks if the second element is the same as the third one from the back
             if(token[1] == token[n - 3])
             {
-                temp = true;
+                // Puts it inside a static array of 256 so that we can
+                // put elements that are in one line back into a single line
                 for(int u = 0; u < n; u++)
                 {
                     ARR[index + u] = token[u];
@@ -158,19 +180,24 @@ void parsingInput(char*** out, char** text, int size)
 
                 index += strlen(token);
 
+                // Adds a space after a word
                 ARR[index++] = ' ';                           
             }
 
+            // Moves to the next token
             token = strtok(NULL, " ");
         }
 
+        // Allocates the number of characters inside ARR to a line
         (*out)[i] = malloc(sizeof(char) * strlen(ARR));
 
+        // If there are no elemts that pass the condition a minus is moved into output buffer
         if(strlen(ARR) <= 2)
         {
             (*out)[i] = "-";
         }
 
+        // The elements that pass the condition ar moved into the output buffer
         else 
         {
             for(int u = 0; u < strlen(ARR); u++)
